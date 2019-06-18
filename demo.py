17b2ae4196040b3af2b683cd32d78037de25511d
Minho Ryu <ryumin93@gmail.com>
2019-06-18 10:43:18 +0900

    update

--- demo.py
+++ demo.py
@@ -43,17 +43,17 @@ def main():
         if utterance == "exit":
             break
         k1 = input("Type first Knowledge: ").lower()
-        while len(k1) != 0:
+        while not k1:
             print("Please type first Knowledge.\n")
             k1 = input("Type first Knowledge: ").lower()
 
         k2 = input("Type second Knowledge: ").lower()
-        while len(k2) != 0:
+        while not k2:
             print("Please type first Knowledge.\n")
             k2 = input("Type first Knowledge: ").lower()
 
         k3 = input("Type third Knowledge: ").lower()
-        while len(k3) != 0:
+        while not k3:
             print("Please type first Knowledge.\n")
             k3 = input("Type first Knowledge: ").lower()
 
